0073b4c39fa4d1bec4896ebe30f5fc81ec83227c
rahulgarg1985 <rahul_garg007@yahoo.com>
2015-05-20 10:36:27 +0530

    Updated project files to support NET40 and added xml documents

--- CodeNode.Extention/Properties/AssemblyInfo.cs
+++ CodeNode.Extention/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeNode.Extention")]
+[assembly: AssemblyTitle("CodeNode.Extension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeNode.Extention")]
+[assembly: AssemblyProduct("CodeNode.Extension")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
